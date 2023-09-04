ad3932ca43e70315d73372dd8a9bd98b1a68106c
Natnael Solomon <natnaelsolomon770@gmail.com>
2023-09-04 16:59:51 +0300

    Tanstack configured

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const App = () => {
+  // Create a client
+  const queryClient = new QueryClient();
   return (
-    <div className=''>App</div>
-  )
-}
+    // Provide the client to your App
+    <QueryClientProvider client={queryClient}>
+      <div>Hello</div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
 
-export default App
+export default App;
